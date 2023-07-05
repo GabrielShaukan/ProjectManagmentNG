28fa83e7e1abaacf39355d58031304d304aeb516
gabriel shaukan <gabrielshaukan@yahoo.com>
2023-07-05 09:39:54 +0200

    Implemented routing

--- project-management-ng/src/app/app.module.ts
+++ project-management-ng/src/app/app.module.ts
@@ -6,10 +6,19 @@ import { ProductListComponent } from './products/product-list.component';
 import { FormsModule } from '@angular/forms';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent, ProductDetailComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot([
+    {path:'products', component: ProductListComponent},
+    {path:'products/:id', component: ProductDetailComponent},
+    {path:'welcome', component: WelcomeComponent},
+    {path:'', redirectTo: 'welcome', pathMatch: 'full'},
+    {path:'**', redirectTo: 'welcome', pathMatch: 'full'},
+  ])],
   providers: [],
   bootstrap: [AppComponent],
 })
--- project-management-ng/src/app/products/product-list.component.ts
+++ project-management-ng/src/app/products/product-list.component.ts
@@ -5,7 +5,6 @@ import { ProductsService } from './product.service';
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
